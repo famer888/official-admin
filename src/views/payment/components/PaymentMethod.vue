ed03893ac2af805806bc5d57aba92c4e56554fc4
wente <a7@A7deMacBook-Air.local>
2026-02-09 22:26:32 +0800

    fix: merging

--- src/views/payment/components/PaymentMethod.vue
+++ src/views/payment/components/PaymentMethod.vue
@@ -4,8 +4,12 @@
     <div class="grid grid-cols-2 gap-4">
       <template v-for="method in payMethod" :key="method.value">
         <div
-          class="flex gap-2 items-center px-4 h-[42px] text-left text-[#1D2129] text-[16px] bg-[#F4F5F9] rounded-[4px] cursor-pointer"
-          :class="{ 'bg-[#3A82F9] text-[#fff]': current === method.value }"
+          :class="{
+            'bg-[#3A82F9]': current === method.value,
+            'text-[#fff]': current === method.value,
+            'bg-[#F4F5F9]': current !== method.value,
+          }"
+          class="flex gap-2 items-center px-4 h-[42px] text-left text-[#1D2129] text-[16px] rounded-[4px] cursor-pointer"
           @click="handleChange(method.value)"
         >
           <img :src="method.icon" alt="" />
