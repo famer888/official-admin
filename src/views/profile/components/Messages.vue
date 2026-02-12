fca4598690ee504478eec4b754f2d4aaf102b073
leo <leo>
2026-02-12 11:23:55 +0800

    feat: 修改密码

--- src/views/profile/components/Messages.vue
+++ src/views/profile/components/Messages.vue
@@ -4,19 +4,7 @@
       <n-list-item v-for="item in messageList" :key="item.messageId">
         <div class="message-item" @click="handleMarkRead(item)">
           <div class="message-icon">
-            <n-icon
-              :size="24"
-              :color="getMessageIconColor(item.bizType)"
-              v-if="getMessageType(item.bizType) === 'warning'"
-            >
-              <WarningOutlined />
-            </n-icon>
-            <n-icon :size="24" color="#18a058" v-else-if="getMessageType(item.bizType) === 'info'">
-              <InfoCircleOutlined />
-            </n-icon>
-            <n-icon :size="24" color="#2080f0" v-else>
-              <BellOutlined />
-            </n-icon>
+            <img :src="getMessageType(item.bizType)" />
           </div>
           <div class="message-content">
             <div class="message-text">{{ item.message }}</div>
@@ -47,7 +35,10 @@
   import { ref, onMounted } from 'vue'
   import { getMessageList, markMessageRead, getUnreadCount } from '../useApi'
   import { WarningOutlined, InfoCircleOutlined, BellOutlined } from '@vicons/antd'
-
+  import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
+  import msgType from '@/assets/svgIcon/msg-type.svg'
+  import msgType1 from '@/assets/svgIcon/msg-type2.svg'
+  import msgType2 from '@/assets/svgIcon/msg-type3.svg'
   const emit = defineEmits(['update:unread-count'])
 
   const messageList = ref([])
@@ -60,17 +51,12 @@
 
   // 根据 bizType 获取消息类型
   const getMessageType = (bizType) => {
-    // 根据业务类型判断消息类型，可以根据实际业务调整
-    // 1: warning, 2: info, 其他: default
-    if (bizType === 1) return 'warning'
-    if (bizType === 2) return 'info'
-    return 'default'
-  }
-
-  // 获取消息图标颜色
-  const getMessageIconColor = (bizType) => {
-    const type = getMessageType(bizType)
-    return type === 'warning' ? '#f53f3f' : '#18a058'
+    let iconMap = {
+      1: msgType,
+      2: msgType1,
+      3: msgType2,
+    }
+    return iconMap[bizType]
   }
 
   // 加载未读消息数量
