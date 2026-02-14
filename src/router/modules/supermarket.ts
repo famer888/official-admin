2193d8697d6e348c6e40d86ce05f6d03cd8f1689
leo <leo>
2026-02-14 17:11:48 +0800

    feat: 图标替换更新

--- src/router/modules/supermarket.ts
+++ src/router/modules/supermarket.ts
@@ -1,6 +1,6 @@
 import { RouteRecordRaw } from 'vue-router'
 import { Layout } from '@/router/constant'
-import { SettingOutlined } from '@vicons/antd'
+import { FileSearchOutlined } from '@vicons/antd'
 import { renderIcon } from '@/utils/index'
 // 系统设置
 const routes: Array<RouteRecordRaw> = [
@@ -11,7 +11,7 @@ const routes: Array<RouteRecordRaw> = [
     component: Layout,
     meta: {
       title: '广告超市',
-      icon: renderIcon(SettingOutlined),
+      icon: renderIcon(FileSearchOutlined),
       sort: 3,
     },
     children: [
