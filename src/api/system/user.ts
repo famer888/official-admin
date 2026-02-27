f26da4a3475ea8fde6aef2358051e7f1421f4471
afeng <zhanghuiyu997@gmail.com>
2026-02-27 18:32:21 +0800

    获取用户信息接口替换

--- src/api/system/user.ts
+++ src/api/system/user.ts
@@ -4,7 +4,8 @@ import { Alova } from '@/utils/http/alova/index'
  * @description: 获取用户信息
  */
 export function getUserInfo() {
-  return Alova.Get<InResult>('/admin-api/system/menu/getPermissionInfo', { cacheFor: null })
+  // return Alova.Get<InResult>('/admin-api/system/menu/getPermissionInfo', { cacheFor: null })
+  return Alova.Get<InResult>('/proxy/menu/getPermissionInfo', { cacheFor: null })
 }
 /**
  *
