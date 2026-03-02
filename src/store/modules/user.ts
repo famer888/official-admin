a9f97adc7f395cd25c96a4b03af7e1fd97c5ba25
leo <leo>
2026-03-02 20:01:19 +0800

    feat: 更新个人中心

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -123,14 +123,11 @@ export const useUserStore = defineStore({
       return response
     },
     async getInfoByAuth() {
-      if (import.meta.env.DEV) {
-        return
-      }
-      await getUserInfoByAuth()
-      // console.log('🚀 ~ response:', response)
-      // if (response.code === ResultEnum.SUCCESS) {
-      //   this.setUserInfo(response.data)
+      // if (import.meta.env.DEV) {
+      //   return
       // }
+      // await getUserInfoByAuth()
+      this.getInfo()
     },
 
     // 获取用户信息
@@ -191,7 +188,7 @@ export const useUserStore = defineStore({
           storage.remove(CURRENT_USER)
           await this.getInfoByAuth()
         }
-      } catch (error) { }
+      } catch (error) {}
     },
   },
 })
@@ -201,7 +198,7 @@ export function useUser() {
   const userStore = useUserStore(store)
   // 设置全局引用，供 useGlobSetting 使用，避免循环依赖
   if (typeof window !== 'undefined') {
-    (window as any).__userStore = userStore
+    ;(window as any).__userStore = userStore
   }
   return userStore
 }
