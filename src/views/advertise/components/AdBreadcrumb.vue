2e78b2cab73409b8a406681a55468099f8634c0f
fofo <fofo@gmail.com>
2026-03-14 17:15:34 +0800

    fix: svg处理修改

--- src/views/advertise/components/AdBreadcrumb.vue
+++ src/views/advertise/components/AdBreadcrumb.vue
@@ -6,20 +6,14 @@
     >
       {{ parentTitle }}
     </span>
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M11.3535 0.353516L0.353516 11.3535" stroke="#3A82F9" />
-    </svg>
+    <SvgIcon name="breadcrumb-slash" :size="12" color="#3A82F9" />
     <span class="text-[#3A82F9]">{{ currentTitle }}</span>
   </div>
 </template>
 
 <script setup>
+  import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
+
   defineProps({
     parentTitle: { type: String, default: '广告管理' },
     currentTitle: { type: String, required: true },
